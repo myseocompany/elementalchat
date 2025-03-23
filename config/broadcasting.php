582d7637baf643deeb15f6b4c107eab2373b79fb
Nicolás Navarro <nicolas@myseocompany.co>
2025-03-23 09:28:43 -0500

    old broadcast

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -15,8 +15,8 @@ return [
     |
     */
 
-    //'default' => env('BROADCAST_CONNECTION', 'null'),
-    'default' => env('BROADCAST_DRIVER', 'null'),
+    'default' => env('BROADCAST_CONNECTION', 'null'),
+    //'default' => env('BROADCAST_DRIVER', 'null'),
 
     /*
     |--------------------------------------------------------------------------
