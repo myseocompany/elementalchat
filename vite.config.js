84d9fa463b669e04e131e7c237c2665b863f3579
Nicolás Navarro <nicolas@myseocompany.co>
2025-03-24 07:55:22 -0500

    corregir vite2

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite';
-
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
@@ -11,6 +10,5 @@ export default defineConfig({
             ],
             refresh: true,
         }),
-        tailwindcss(),
     ],
 });
