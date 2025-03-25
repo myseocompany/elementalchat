353483f02c699ecbc22ab7b3ad8aee74da3a6b50
Nicolás Navarro <nicolas@myseocompany.co>
2025-03-24 19:51:12 -0500

    add orderFile Model

--- app/Http/Controllers/OrderFileController.php
+++ app/Http/Controllers/OrderFileController.php
@@ -2,7 +2,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\OrderFile;
+use App\Models\OrderFile;
 use Illuminate\Support\Facades\Storage;
 
 class OrderFileController extends Controller
